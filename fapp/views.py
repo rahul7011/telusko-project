7fc3f754ca412bafd1e47cea9c8f92a3db593137
rahul7011 <champrahul01@gmail.com>
2019-07-02 14:17:28 +0530

    Printing hello world from the fapp

--- fapp/views.py
+++ fapp/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render             #importing renderer support
+from django.http import HttpResponse            #importing html support
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse('Hello World!')
