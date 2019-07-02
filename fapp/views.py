71cab9a9b610dc41912ee782f18433822f7c3fae
rahul7011 <champrahul01@gmail.com>
2019-07-02 16:58:40 +0530

    changing to post method and using csrk token to prevent attack(neccesary)

--- fapp/views.py
+++ fapp/views.py
@@ -7,7 +7,7 @@ def home(request):
     return render(request, 'index.html')
 
 def addition(request):
-    val1 = int(request.GET['num1'])
-    val2 = int(request.GET['num2'])
+    val1 = int(request.POST['num1'])
+    val2 = int(request.POST['num2'])
     result=val1 + val2
     return render(request,'result.html',{'result':result})
