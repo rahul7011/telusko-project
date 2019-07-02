9ab1c3f8037f321a102baef19875693ae3ec9ed8
rahul7011 <champrahul01@gmail.com>
2019-07-02 16:50:07 +0530

    addition of two numbers and displaying result under new file(template)

--- fapp/urls.py
+++ fapp/urls.py
@@ -3,4 +3,5 @@ from .import views                                #where views is a file
 
 urlpatterns = [
     path('', views.home, name ='home'),
+    path('add', views.addition, name ='add'),
 ]
--- fapp/views.py
+++ fapp/views.py
@@ -5,3 +5,9 @@ from django.http import HttpResponse            #importing html support
 
 def home(request):
     return render(request, 'index.html')
+
+def addition(request):
+    val1 = int(request.GET['num1'])
+    val2 = int(request.GET['num2'])
+    result=val1 + val2
+    return render(request,'result.html',{'result':result})
