f96d574e265fac576ac55c50529ca23d47a0b7f5
rahul7011 <champrahul01@gmail.com>
2019-07-03 00:41:37 +0530

    from static to dynamic website part2

--- travello/models.py
+++ travello/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class destination:
+    name:str
+    img:str
+    price:int
+    desc:str
--- travello/views.py
+++ travello/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from .models import destination
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    dest1=destination()
+    dest1.name="Delhi"
+    dest1.price=600
+    dest1.desc="city of love and power"
+
+    dest2=destination()
+    dest2.name="Mumbai"
+    dest2.price=400
+    dest2.desc="city which never sleeps"
+
+    dest3=destination()
+    dest3.name="Banglore"
+    dest3.price=605
+    dest3.desc="silicon valley"
+
+
+
+
+    return render(request,'index.html',{'dest1':dest1,'dest2':dest2,'dest3':dest3})
